252a50f52e74833a51499db045902e138b53c6d4
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-07-08 15:56:38 -0700

    quick change

--- includes/Collidable.h
+++ includes/Collidable.h
@@ -12,16 +12,21 @@ public:
 	Collidable(Collidable const &ref);
 	~Collidable();
 
+	void			setSprite(std::string const sprite);
 	Place			*getLoc() const;
 	std::string		getSprite() const;
 
-	void			move(int xdif, int ydif);
+	void			move();
 
 	Collidable		&operator =(Collidable const &rhs);
 
-private:
+protected:
 	Place			*_location;
 	std::string		_sprite;
+	float			_x;
+	float			_y;
+	float			_dx;
+	float			_dy;
 }
 
 #endif
---
+++ includes/Place.h
@@ -0,0 +1,26 @@
+
+#ifndef PLACE_H
+# define PLACE_H
+
+class				Place
+{
+public:
+
+	Place();
+	Place(unsigned int x, unsigned int y, Collidable *obj = 0);
+	Place(Place const &ref);
+	~Place();
+
+	Collidable		*getObj() const;
+	unsigned int	getX() const;
+	unsigned int	getY() const;
+
+	Place			&operator =(Place const &rhs);
+
+private:
+	Collidable		*_obj;
+	unsigned int	_x;
+	unsigned int	_y;
+}
+
+#endif
