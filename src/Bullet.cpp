5c94f08ebab4213d050fedc4fcf0a06235a11ba7
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-07-09 16:00:53 -0700

    merging

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -4,8 +4,8 @@
 
 Bullet::Bullet(void) : Collidable::Collidable()
 {
-	this->setSprite('o');
-	this->setSpeed(0.0, -1.0);
+	this->setSprite('O');
+	this->setSpeed(0.0, -1.2);
 	this->_damage = 1;
 }
 
