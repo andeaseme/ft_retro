2c5a185c562864d722fe17c9ccc2c470a6559d61
Andie Phan <aphan@student.42.us.org>
2017-07-08 22:24:00 -0700

    signal catch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 
 #include "Level.h"
+#include <signal.h>
+
+void
+termination_handler (int signum)
+{
+	endwin();
+	std::exit(1);
+}
 
 int				main()
 {
+	struct sigaction action;
+
+	action.sa_handler = termination_handler;
+	sigaction(SIGSEGV, &action, NULL);
 	Level::init();
 	Level::loop();
 	endwin();
